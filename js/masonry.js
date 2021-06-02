31f43bba6cf98c9950ac4dee8ce2d5876be8d9a1
Olena Savchenko <olsavchenko1@gmail.com>
2021-06-02 22:05:46 +0300

    reworked gallery

--- js/masonry.js
+++ js/masonry.js
@@ -1,9 +1,14 @@
 "use strict";
 
-const $grid = $(".grid").imagesLoaded(function () {
-  $grid.masonry({
+const container = document.querySelector(".grid");
+let msnry;
+
+imagesLoaded(container, function () {
+  msnry = new Masonry(container, {
     columnWidth: ".grid-sizer",
     itemSelector: ".grid-item",
     gutter: ".gutter-sizer",
+    presentPosition: true,
+    singleMode: false,
   });
 });
